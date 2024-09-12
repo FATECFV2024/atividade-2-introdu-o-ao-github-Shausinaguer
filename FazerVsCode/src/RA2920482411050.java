b188b85f32c57e406b9d0dc1eb31f1c350723af1
Shausinaguer <149312021+Shausinaguer@users.noreply.github.com>
2024-09-11 21:12:01 -0300

    [Feat] Adicionando Atributos
    Nome Completo,RA,Idade,Seu @gitHub,Seu @LinkedIn.Curso e Turma adicionados em forma de comentário ao lado de seu respectivo atributo.

---
+++ FazerVsCode/src/RA2920482411050.java
@@ -0,0 +1,12 @@
+public class RA2920482411050 {
+    private String nomecompleto;
+    //Nicolas Felipe De Carvalho
+    private int idade;
+    //18 anos
+    private String SeuGithub;
+    //https://github.com/Shausinaguer
+    private String SeuLinkedIn;
+    //https://www.linkedin.com/in/nicolas-f-carvalho/
+    private String Curso_e_Turma;
+    //ADS - 2° Semestre
+}
